069d0507cfefb01df91f10354ea4557380d50939
yzsd_chenshengbin <chensb@emspost.com.cn>
2023-09-11 16:41:48 +0800

    目录调整

--- README.md
+++ README.md
@@ -5,10 +5,7 @@ build with [docsify](https://docsify.js.org/#/quickstart )
 ## cmd
 
 ```
+npm i docsify-cli -g
+
 docsify serve ./
 ```
-
-## gist test
-
-- [gist: AsyncStream.java](https://gist.github.com/thinban/a76c79416fbfc1e03a3f3f5c814e76c4 ':include :type=code')
-- [gist: raw,AsyncStream.java](https://gist.githubusercontent.com/thinban/a76c79416fbfc1e03a3f3f5c814e76c4/raw/b69f42dd313613fa7647cb45a51a6b8b3416be61/AsyncStream.java ':include :type=code')
--- menu.go
+++ menu.go
@@ -27,12 +27,12 @@ func recur(dir string, num int) {
 		name := fi.Name()
 		d := dir + `/` + name
 		if fi.IsDir() && !strings.Contains(ignore, name) {
-			s = s + strings.Repeat("*", num+1) + " " + fmt.Sprintf(tpl, name, "") + "\n"
+			s = s + strings.Repeat("\t", num) + "* " + fmt.Sprintf(tpl, name, "") + "\n"
 			recur(d+`/`, num+1)
 		} else {
 			if num > 0 {
 				d = strings.ReplaceAll(d, "//", "/")
-				s = s + strings.Repeat("*", num+1) + " " + fmt.Sprintf(tpl, name, d[1:]) + "\n"
+				s = s + strings.Repeat("\t", num) + "* " + fmt.Sprintf(tpl, name, d[1:]) + "\n"
 				//println(name)
 			}
 		}
