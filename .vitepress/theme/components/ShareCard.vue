ee92c57917b9e34fbc5108a295f0977f6fbf654b
yzsd_chenshengbin <chensb@emspost.com.cn>
2023-12-13 10:53:19 +0800

    'init'

---
+++ .vitepress/theme/components/ShareCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="shareCard">
+    <h1 class="title">
+      <span>Hi, I'm ThinBan 👋</span>
+    </h1>
+    <p class="description">
+      <!-- A frontend developer(he/him) who is building in web3. -->
+      A GuangDong 靓仔.
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+<style scoped>
+.shareCard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 15px;
+  text-align: center;
+}
+.title {
+  display: flex;
+  align-items: center;
+  font-size: 2.2em;
+  font-weight: bold;
+}
+.description {
+  margin: 1em 0;
+}
+</style>
